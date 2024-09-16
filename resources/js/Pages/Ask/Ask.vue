4bb6eae3e5997e425308d5cac567435cbfb2598e
Matthieu Driss <matthieu.driss@gmail.com>
2024-09-17 00:43:20 +0200

    ok responsive ask

--- resources/js/Pages/Ask/Ask.vue
+++ resources/js/Pages/Ask/Ask.vue
@@ -40,20 +40,20 @@ const onInstantQuestion = (question_answer) => {
 
 <template>
     <div class="custom-background-color flex flex-col">
-        <div class="py-12">
-            <h2 class="text-5xl font-semibold text-white justify-center flex ">{{ props.team.parameters.title }}</h2>
-            <div class="w-3/4 mx-auto mt-6 ">
-            <label class="flex flex-col-reverse relative focus group w-3/5 mx-auto">
-                <div class="relative flex px-3">
-                    <Question @question="onQuestion" :channel="props.channel" :team="props.team" />
-                </div>
-            </label>
-        </div>
+        <div class="py-16 lg:py-12">
+            <h2 class="text-3xl text-center lg:text-5xl font-semibold text-white justify-center flex ">{{ props.team.parameters.title }}</h2>
+            <div class="lg:w-3/4 mx-auto mt-6 ">
+                <label class="flex flex-col-reverse relative focus group lg:w-3/5 mx-auto">
+                    <div class="relative flex px-3">
+                        <Question @question="onQuestion" :channel="props.channel" :team="props.team" />
+                    </div>
+                </label>
+            </div>
         </div>
     </div>
 
-    <div class="mt-12 w-3/4 mx-auto flex">
-        <div class="grid grid-cols-3 gap-x-8 w-full h-96">
+    <div class="mt-6 lg:mt-12 w-full lg:w-3/4 mx-auto flex">
+        <div class=" lg:grid grid-cols-3 gap-x-8 w-full h-96">
             <div class="h-full col-span-2 w-full p-3">
                 <Answer :answer="answer" :question="question" :asking="asking"/>
                 <Vote :team="props.team" :answer_id="answer_id" />
