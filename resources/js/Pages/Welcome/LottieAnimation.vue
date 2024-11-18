ce62364c1b8abd6e69b95720b5ca8355a4f8a859
Matthieu Driss <matthieu.driss@gmail.com>
2024-11-19 00:30:57 +0100

    landing page ebauche

---
+++ resources/js/Pages/Welcome/LottieAnimation.vue
@@ -0,0 +1,24 @@
+<!-- LottieAnimation.vue -->
+<template>
+    <div id="loading-animation">
+    </div>
+</template>
+
+<script setup>
+import bodymovin from "lottie-web";
+import { onMounted, ref } from 'vue';
+
+onMounted(() => {
+    bodymovin.loadAnimation({
+        container: document.getElementById('loading-animation'),
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: '/storage/lotties/hero.json',
+    })
+});
+</script>
+
+<style scoped>
+/* Styles personnalisés si nécessaire */
+</style>
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,12 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+
+    ]);
+});
+
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified', Subscribed::class, LockedMiddleware::class])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 });
